c7b091e269125c84938ad32e12cdc3df6fc31182
Om Subham Pradhan <omsubhampradhan@gmail.com>
2026-07-02 22:29:33 +0530

    added categories

---
+++ categories.py
@@ -0,0 +1,32 @@
+IMAGES = [
+".jpg",
+".png",
+".jpeg",
+".gif"
+]
+
+VIDEOS = [
+".mp4",
+".avi",
+".mkv"
+]
+
+MUSIC = [
+".mp3",
+".wav"
+]
+
+DOCUMENTS = [
+".pdf",
+".txt",
+".docx"
+]
+
+PYTHON = [
+".py"
+]
+
+ARCHIVES = [
+".zip",
+".rar"
+]
